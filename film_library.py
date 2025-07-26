176d61bdbcf2471a04819ffb0a22133339870577
Michal Mordarski <mmordarski4@gmail.com>
2025-07-26 12:38:15 +0200

    Add getters and search function

--- film_library.py
+++ film_library.py
@@ -21,3 +21,23 @@ class Series(Film):
     def __str__(self):
         return f'{self.title} S{self.season_num:02}E{self.episode_num:02}'
     
+def get_objects(list, object_class):
+    objects = []
+    for i in list:
+        if type(i) == object_class:
+            objects.append(i)
+    objects.sort(key = lambda x: x.title)
+    return objects
+
+def get_movies(list):
+    return get_objects(list, Film)
+
+def get_series(list):
+    return get_objects(list, Series)
+
+def search(title,list):
+    for i in list:
+        if i.title == title:
+            print(f'Film o tytule {title} znajduje się w bibliotece')
+        else:
+            print(f'Film o tytule {title} nie znajduje się w bibliotece')
